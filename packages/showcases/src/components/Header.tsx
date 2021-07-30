ba4c770364684fb63b844039aa5004c660671428
Sergiy Chemodanov <sergiychemodanov@gmail.com>
2021-07-30 09:36:18 -0500

    feat: header and sidebar

--- packages/showcases/src/components/Header.tsx
+++ packages/showcases/src/components/Header.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import logo from "../images/logo.svg";
+import slack from "../images/slack.svg";
+import github from "../images/github.svg";
+
+type HeaderLinkProps = {
+  url: string;
+  children: React.ReactNode;
+  icon: React.ReactNode;
+};
+
+const HeaderItem = ({ icon, children, url }: HeaderLinkProps) => (
+  <a
+    className="flex flex-row items-center ml-8"
+    href={url}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {icon}
+    <div className="ml-3">{children}</div>
+  </a>
+);
+
+const linkItems = [
+  {
+    url: "https://get.jina.ai",
+    text: "get.jina.ai",
+    icon: <Image src={github} alt="Github" />,
+  },
+  {
+    url: "https://slack.jina.ai",
+    text: "slack.jina.ai",
+    icon: <Image src={slack} alt="Slack" />,
+  },
+];
 
 export const Header = () => {
-	return (
-		<div className="bg-gray-100 sticky p-4">
-			This is a header
-		</div>
-	)
-}
+  return (
+    <div className="border-b-2 border-gray-100 sticky px-8 py-4 flex flex-row items-center">
+      <div className="flex-1">
+        <Image src={logo} alt="Jina" />
+      </div>
+      {linkItems.map(({ url, icon, text }, idx) => (
+        <HeaderItem icon={icon} url={url} key={idx}>
+          {text}
+        </HeaderItem>
+      ))}
+    </div>
+  );
+};
--- packages/showcases/src/components/Sidebar.tsx
+++ packages/showcases/src/components/Sidebar.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
+
+type SidebarItemProps = {
+  url: string;
+  children: React.ReactNode;
+  selected: boolean;
+};
+const SidebarItem = ({ children, url, selected }: SidebarItemProps) => {
+  return (
+    <a
+      className={`block text-gray-600 my-8 p-2${
+        selected ? " font-bold text-primary-500" : ""
+      }`}
+      href={url}
+    >
+      {children}
+    </a>
+  );
+};
+
+const items = [
+  { url: "/pdf", title: "PDF Showcase" },
+  { url: "/e-commerce", title: "E-commerce Showcase" },
+  { url: "/gaming", title: "Gaming Showcase" },
+];
 
 export const Sidebar = () => {
-	return (
-		<div className="bg-gray-50 w-64 p-4">
-			This is a sidebar
-		</div>
-	)
-}
+  const { asPath } = useRouter();
+  return (
+    <div className="bg-gray-50 w-72 px-6">
+      {items.map(({ url, title }) => (
+        <SidebarItem url={url} selected={url === asPath} key={url}>
+          {title}
+        </SidebarItem>
+      ))}
+    </div>
+  );
+};
