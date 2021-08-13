e13eb5b9618a5d973b64dc3c530c4d0531f7e1dc
Sergiy Chemodanov <sergiychemodanov@gmail.com>
2021-08-13 10:56:32 -0500

    fix: set flow url

--- packages/showcases/src/components/SearchDemo.tsx
+++ packages/showcases/src/components/SearchDemo.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import JinaClient, { SimpleResults } from "@jina-ai/jinajs";
+import JinaClient, { BaseURL, SimpleResults } from "@jina-ai/jinajs";
 import { Spinner } from "./Spinner";
-const jina = new JinaClient("http://localhost:45678");
 
 export const SearchDemo = () => {
+  const [url, setURL] = useState("");
   const [text, setText] = useState("");
+  const [jina, setJina] = useState<JinaClient>();
   const [results, setResults] = useState<SimpleResults>([]);
   const [file, setFile] = useState<File>();
   const [searching, setSearching] = useState(false);
@@ -15,7 +16,12 @@ export const SearchDemo = () => {
     setFile(file);
   };
 
+  async function initClient() {
+    setJina(new JinaClient(url as BaseURL));
+  }
+
   async function search() {
+    if (!jina) return;
     setSearching(true);
     let query = file || text;
     const results = await jina.search(query);
@@ -28,33 +34,55 @@ export const SearchDemo = () => {
       <div className="border-b text-2xl font-medium mb-4">
         Search Demo (single document query)
       </div>
-      <div className="flex flex-row items-center">
-        <input
-          className="border px-4 py-2 rounded"
-          onChange={(e) => setText(e.target.value)}
-          placeholder="search..."
-        />
-        <div className="px-2">
-          <input
-            type="file"
-            className="mt-4"
-            id="attach-files-button"
-            onChange={(e) => handleFileSelect(e.target.files)}
-          />
+      {jina ? (
+        <div>
+          <div className="flex flex-row items-center">
+            <input
+              className="border px-4 py-2 rounded"
+              onChange={(e) => setText(e.target.value)}
+              placeholder="search..."
+            />
+            <div className="px-2">
+              <input
+                type="file"
+                className="mt-4"
+                id="attach-files-button"
+                onChange={(e) => handleFileSelect(e.target.files)}
+              />
+            </div>
+            <button
+              disabled={searching}
+              className="rounded p-2 bg-green-500 px-6 text-white ml-3"
+              onClick={search}
+            >
+              Search
+            </button>
+            <div className="text-green-500 ml-4">
+              {searching && <Spinner />}
+            </div>
+          </div>
+          <div className="my-4">Raw Results:</div>
+          <div className="bg-gray-50 rounded p-4 break-words font-mono">
+            {results.length
+              ? JSON.stringify(results)
+              : "results will display here"}
+          </div>
         </div>
-        <button
-          disabled={searching}
-          className="rounded p-2 bg-green-500 px-6 text-white ml-3"
-          onClick={search}
-        >
-          Search
-        </button>
-        <div className="text-green-500 ml-4">{searching && <Spinner />}</div>
-      </div>
-      <div className="my-4">Raw Results:</div>
-      <div className="bg-gray-50 rounded p-4 break-words font-mono">
-        {results.length ? JSON.stringify(results) : "results will display here"}
-      </div>
+      ) : (
+        <div className="flex flex-row items-center">
+          <input
+            className="border px-4 py-2 rounded"
+            onChange={(e) => setURL(e.target.value)}
+            placeholder="endpoint URL"
+          />
+          <button
+            className="rounded p-2 bg-green-500 px-6 text-white ml-3"
+            onClick={initClient}
+          >
+            Set
+          </button>
+        </div>
+      )}
     </div>
   );
 };
