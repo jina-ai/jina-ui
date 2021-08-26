21bd91f3f0448c5a0c272095844d56c2cec67e5e
Roshan Jossy <roshan.jossy@jina.ai>
2021-08-27 01:53:53 +0200

    fix styles for flowchart

--- packages/showcases/src/components/FlowDiagram.tsx
+++ packages/showcases/src/components/FlowDiagram.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {useRouter} from 'next/router'
 import SampleFlow from "../images/sampleflow.svg";
 import PDFFlow from "../images/pdf-search-flowchart.svg"
 import ECommerceFlow from "../images/e-commerce-flowchart.svg"
@@ -16,6 +17,8 @@ const getFlowChartAsset = (showcase: string) => {
 
 export const FlowDiagram = () => {
   const [show, setShow] = useState(false);
+  const {asPath} = useRouter()
+  const path = asPath.replace('/', '')
   function toggleShow() {
     setShow((prev) => !prev);
   }
@@ -35,8 +38,8 @@ export const FlowDiagram = () => {
           show ? "max-h-screen" : "max-h-0"
         }`}
       >
-        <div className="flex justify-center p-4 bg-white rounded mt-4 overflow-y-auto">
-          <Image src={getFlowChartAsset('pdf')} alt="flow" />
+        <div className="flex justify-center pb-8 bg-gray-100 rounded mt-4 overflow-y-auto">
+          <Image src={getFlowChartAsset(path)} alt="flow" />
         <div className="justify-self-end self-end  mt-4 flex flor-row items-center cursor-pointer hover:bg-gray-200 p-2 rounded-md tranistion-all duration-200">
           <Image src={github} alt="github" />
           <span className="ml-2">View on Github</span>
