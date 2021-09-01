a96fe229b0660185cf2c1348ad00c556a456c308
Sergiy Chemodanov <sergiychemodanov@gmail.com>
2021-09-01 08:20:41 -0500

    feat: pdf preview

--- packages/showcases/src/components/common/FilePreview.tsx
+++ packages/showcases/src/components/common/FilePreview.tsx
@@ -1,9 +1,16 @@
-import { XIcon } from '@heroicons/react/solid';
-import React, { useEffect, useState } from 'react'
-import { CompactCaption } from './CompactCaption';
-import { MediaPreview } from './MediaPreview';
+import { TrashIcon } from "@heroicons/react/outline";
+import { XIcon } from "@heroicons/react/solid";
+import React, { useEffect, useState } from "react";
+import { CompactCaption } from "./CompactCaption";
+import { MediaPreview } from "./MediaPreview";
 
-export const FilePreview = ({ file, remove }: { file: File; remove?: () => void }) => {
+export const FilePreview = ({
+  file,
+  remove,
+}: {
+  file: File;
+  remove?: () => void;
+}) => {
   const [src, setSrc] = useState("");
 
   useEffect(() => {
@@ -15,18 +22,20 @@ export const FilePreview = ({ file, remove }: { file: File; remove?: () => void 
   }, [file]);
 
   return (
-    <div className="h-full bg-gray-50 w-40 flex flex-col rounded relative mr-2 select-none">
-      {remove && (
-        <XIcon
-          className="h-4 absolute -right-1.5 -top-1.5 cursor-pointer"
-          onClick={remove}
-        />
-      )}
-      <div className="flex-1 overflow-hidden flex items-center">
+    <div className="flex-none h-full bg-gray-50 w-72 flex flex-col rounded relative mr-2 select-none">
+      <div className="flex-1 overflow-hidden flex items-center px-2 pt-2">
         <MediaPreview mimeType={file.type} src={src} />
       </div>
-      <div className="p-1">
+      <div className="p-1 px-2 flex flex-row items-center">
         <CompactCaption text={file.name} />
+        <div className="flex-1 text-right">
+          {remove && (
+            <TrashIcon
+              className="h-4 inline cursor-pointer text-gray-700 hover:text-black"
+              onClick={remove}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
--- packages/showcases/src/components/common/MediaPreview.tsx
+++ packages/showcases/src/components/common/MediaPreview.tsx
@@ -1,6 +1,69 @@
-import React from "react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/outline";
+import React, { ReactNode, useCallback, useState } from "react";
+import { Document, Page } from "react-pdf";
 
 /* eslint @next/next/no-img-element: "off" */
+
+const PageButton = ({
+  next,
+  canAct,
+  action,
+}: {
+  next?: boolean;
+  canAct: boolean;
+  action: () => void;
+}) => {
+  return (
+    <div
+      className={`${next ? "" : "ml-auto"} ${
+        canAct ? "" : "opacity-40"
+      } h-6 w-6 bg-white bg-opacity-80 shadow-md p-1 rounded-full transition-all duration-200 cursor-pointer`}
+      onClick={canAct ? action : undefined}
+    >
+      {next ? <ArrowRightIcon /> : <ArrowLeftIcon />}
+    </div>
+  );
+};
+
+const PdfPreview = ({ src }: { src: string }) => {
+  const [width, setWidth] = useState(0);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const container = useCallback((node) => {
+    if (node) setWidth(node.getBoundingClientRect().width - 20);
+  }, []);
+
+  function onLoad({ numPages }: any) {
+    setTotalPages(numPages);
+  }
+
+  function nextPage() {
+    setCurrentPage((prev) => prev + 1);
+  }
+
+  function prevPage() {
+    setCurrentPage((prev) => prev - 1);
+  }
+
+  const hasPrevPage = currentPage > 1;
+  const hasNextPage = currentPage < totalPages;
+
+  return (
+    <div className="h-full w-full" ref={container}>
+      <div className="overflow-y-auto w-full h-full">
+        <Document file={src} onLoadSuccess={onLoad}>
+          <Page pageNumber={currentPage} width={width} />
+        </Document>
+      </div>
+
+      <div className="absolute h-8 left-0 right-0 bottom-0 grid grid-cols-2 gap-2 select-none">
+        <PageButton action={prevPage} canAct={hasPrevPage} />
+        <PageButton action={nextPage} canAct={hasNextPage} next />
+      </div>
+    </div>
+  );
+};
 
 export const MediaPreview = ({
   mimeType,
@@ -26,11 +89,21 @@ export const MediaPreview = ({
     return (
       <video
         src={src}
-        className={`min-h-12 min-2-12 max-h-full max-w-full mx-auto${
+        controls
+        muted
+        autoPlay
+        loop
+        className={`min-h-12 min-w-12 max-h-full max-w-full mx-auto ${
           rounded ? " rounded" : ""
         }`}
       />
     );
+
+  if (mimeType?.startsWith("audio") || src.startsWith("data:audio"))
+    return <audio controls src={src} className="w-full m-1" />;
+
+  if (mimeType === "application/pdf" || src.startsWith("data:application/pdf"))
+    return <PdfPreview src={src} />;
 
   // TODO: fallback option
   return null;
