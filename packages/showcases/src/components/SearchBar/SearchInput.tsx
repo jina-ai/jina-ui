8061ec0b6226237d6462725559076563ffba7eb4
BastinJafari <bastinjafari@gmail.com>
2021-09-01 14:28:52 +0200

    feat: add delete input

--- packages/showcases/src/components/SearchBar/SearchInput.tsx
+++ packages/showcases/src/components/SearchBar/SearchInput.tsx
@@ -1,54 +1,70 @@
-import React, { ChangeEvent, MutableRefObject, useRef } from "react";
+import React, {ChangeEvent, MutableRefObject, useRef} from "react";
 import Image from "next/image";
 import magnifyingGlass from "../../images/magnifyingGlass.svg";
-import imageIcon from "../../images/image.svg";
+import crossIcon from "../../images/cross.svg";
+import fileIcon from "../../images/file.svg"
 
 type InputRef = MutableRefObject<HTMLInputElement | null>;
 
 export const SearchInput = ({
-  inputRef,
-  addFiles,
-}: {
-  inputRef: InputRef;
-  addFiles: (files: File[]) => void;
+                                inputRef,
+                                addFiles,
+                            }: {
+    inputRef: InputRef;
+    addFiles: (files: File[]) => void;
 }) => {
-  const fileRef = useRef<HTMLInputElement>(null);
+    const fileRef = useRef<HTMLInputElement>(null);
 
-  function triggerFileSelect() {
-    fileRef.current?.click();
-  }
+    function triggerFileSelect() {
+        fileRef.current?.click();
+    }
 
-  function handleSelectFiles(e: ChangeEvent<HTMLInputElement>) {
-    const fileList = e.target.files;
-    if (fileList) addFiles(Array.from(fileList));
-  }
+    function deleteInput() {
+        Array.from(document.querySelectorAll("input")).forEach(
+            input => (input.value = "")
+        );
+    }
 
-  return (
-    <div className="flex flex-row items-center h-full">
-      <div className="absolute ml-4 h-6 flex">
-        <Image src={magnifyingGlass} alt="jina" layout="intrinsic" />
-      </div>
-      <div className="absolute right-0 mr-4 mb-0 flex border-l border-gray-400 pl-4 h-6">
-        <Image
-          src={imageIcon}
-          alt="image"
-          layout="intrinsic"
-          className="cursor-pointer"
-          onClick={triggerFileSelect}
-        />
-        <input
-          type="file"
-          multiple
-          ref={fileRef}
-          style={{ display: "none" }}
-          onChange={handleSelectFiles}
-        />
-      </div>
-      <input
-        className="rounded-md w-full h-full pl-16 border-none outline-none focus:shadow-lg transition-all duration-200"
-        placeholder="Search"
-        ref={inputRef}
-      />
-    </div>
-  );
+    function handleSelectFiles(e: ChangeEvent<HTMLInputElement>) {
+        const fileList = e.target.files;
+        if (fileList) addFiles(Array.from(fileList));
+    }
+
+    return (
+        <div className="flex flex-row items-center h-full">
+            <div className="absolute ml-4 h-6 flex">
+                <Image src={magnifyingGlass} alt="jina" layout="intrinsic"/>
+            </div>
+            <div className="absolute right-0 mr-4 mb-0 flex pl-4 h-6">
+                <Image
+                    src={crossIcon}
+                    alt="image"
+                    layout="intrinsic"
+                    className="cursor-pointer"
+                    onClick={deleteInput}
+                />
+                <div className='border-l border-gray-400 mx-2'>
+                </div>
+                <Image
+                    src={fileIcon}
+                    alt="image"
+                    layout="intrinsic"
+                    className="cursor-pointer"
+                    onClick={triggerFileSelect}
+                />
+                <input
+                    type="file"
+                    multiple
+                    ref={fileRef}
+                    style={{display: "none"}}
+                    onChange={handleSelectFiles}
+                />
+            </div>
+            <input
+                className="rounded-md w-full h-full pl-16 border-none outline-none focus:shadow-lg transition-all duration-200"
+                placeholder="Search"
+                ref={inputRef}
+            />
+        </div>
+    );
 };
---
+++ packages/showcases/src/pages/pdf.tsx
@@ -0,0 +1,10 @@
+import {SearchBar} from "../components/SearchBar";
+import {FlowDiagram} from "../components/FlowDiagram";
+
+export default function PDF() {
+    return (
+        <>
+            <SearchBar/>
+            <FlowDiagram/>
+        </>)
+}
