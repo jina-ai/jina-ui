3f84915ca2177ad2c0f938f5e7c242c4b9ac8d98
BastinJafari <bastinjafari@gmail.com>
2021-09-02 17:52:39 +0200

    feat: basic searchBar

--- packages/showcases/src/pages/pdf.tsx
+++ packages/showcases/src/pages/pdf.tsx
@@ -1,10 +1,34 @@
 import {SearchBar} from "../components/SearchBar";
 import {FlowDiagram} from "../components/FlowDiagram";
+import JinaClient, {
+    BaseURL,
+    RawDocumentData,
+    SimpleResults,
+    SimpleQueries,
+} from "@jina-ai/jinajs";
+import { useState } from "react";
 
+const PDF_API_URL = "http://34.89.253.237:80"
 export default function PDF() {
+
+    const [queries, setQueries] = useState<SimpleQueries>([]);
+    const [results, setResults] = useState<SimpleResults[]>([]);
+    const [searching, setSearching] = useState(false);
+
+
+    const jinaClient = new JinaClient(PDF_API_URL)
+
+    async function search(...documents: RawDocumentData[]) {
+        setSearching(true);
+        const { results, queries } = await jinaClient.search(...documents)
+        setSearching(false);
+        setResults(results);
+        setQueries(queries);
+        console.log(results)
+    }
     return (
         <>
-            <SearchBar/>
+            <SearchBar searching={searching} search={search}/>
             <FlowDiagram/>
         </>)
 }
