3c3690c8355a7a39a80758b0d36112959b366453
BastinJafari <bastinjafari@gmail.com>
2021-09-07 11:48:42 +0200

    feat: simple search

--- packages/showcases/src/pages/pdf.tsx
+++ packages/showcases/src/pages/pdf.tsx
@@ -6,9 +6,44 @@ import JinaClient, {
     SimpleResults,
     SimpleQueries,
 } from "@jina-ai/jinajs";
-import { useState } from "react";
+import React, {useState} from "react";
+import {AnyObject, SimpleResponse, SimpleResult} from "../../../jinajs/dist/types";
+import Results from "../components/Results";
 
 const PDF_API_URL = "http://34.89.253.237:80"
+
+const customReqSerializer = (documents: RawDocumentData[], version: string) => {
+    return {
+        "mime_type": "text",
+        "data": "this could be your serach"
+    }
+}
+
+const customResSerializer = (response: AnyObject, version: string) => {
+    const docs = response.data
+    const queries: SimpleQueries = [];
+    const results: SimpleResults[] = [];
+
+    docs.forEach((doc: any) => {
+        queries.push({
+            data: doc.pdf,
+            mimeType: "PDF"
+        })
+    })
+
+    const result = docs.map((doc: any) => {
+        return {
+            data: doc.pdf,
+            score: doc.score,
+            mimeType: "PDF"
+        }
+    })
+
+    results.push(result)
+    return {queries, results} as SimpleResponse
+}
+
+
 export default function PDF() {
 
     const [queries, setQueries] = useState<SimpleQueries>([]);
@@ -16,19 +51,23 @@ export default function PDF() {
     const [searching, setSearching] = useState(false);
 
 
-    const jinaClient = new JinaClient(PDF_API_URL)
+    const jinaClient = new JinaClient(PDF_API_URL, customReqSerializer, customResSerializer)
 
     async function search(...documents: RawDocumentData[]) {
         setSearching(true);
-        const { results, queries } = await jinaClient.search(...documents)
+        const {results, queries} = await jinaClient.search(...documents)
         setSearching(false);
         setResults(results);
         setQueries(queries);
-        console.log(results)
+        console.log("results", results)
+        console.log("queries", queries)
+
     }
+
     return (
         <>
             <SearchBar searching={searching} search={search}/>
+            <Results results={results} queries={queries}/>
             <FlowDiagram/>
         </>)
 }
