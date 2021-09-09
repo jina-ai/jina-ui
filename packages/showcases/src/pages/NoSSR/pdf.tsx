f9a30f5c7d60a6b9f2ba53e4f310e2bad50f9b16
BastinJafari <bastinjafari@gmail.com>
2021-09-09 15:51:03 +0200

    feat: download button

--- packages/showcases/src/pages/NoSSR/pdf.tsx
+++ packages/showcases/src/pages/NoSSR/pdf.tsx
@@ -11,6 +11,8 @@ import JinaClient, {
 } from "@jina-ai/jinajs";
 import React, {useState} from "react";
 import Results from "../../components/Results";
+import downloadButton from '../../images/download-button.svg'
+import Image from "next/image";
 
 const PDF_API_URL = "http://34.89.253.237:80"
 
@@ -63,20 +65,34 @@ export default function PDF() {
 
     const CustomResultItem = (result: CustomResult) => {
         const {thumbnail, pdf_name, pdf, page} = result.result
-
+        const [hovered, setHovered] = useState<boolean>(false)
+        
         return (
-            <div className="cursor-pointer max-w-lg" onClick={() => window.open(pdf, "_blank")}>
-                <div className="rounded-xl border border-primary-500 m-b-3 overflow-hidden h-96">
-                    <img className="thumpnail" src={thumbnail}/>
+            <div className="max-w-lg">
+                <div className="relative rounded-xl border border-primary-500 m-b-3 overflow-hidden h-96"
+                     onMouseOver={() => setHovered(true)}
+                     onMouseLeave={() => setHovered(false)}
+                >
+                    <img className={"cursor-pointer " + (hovered && "thumpnail-hovered ")} src={thumbnail}
+
+                    />
                     <style jsx>
                         {`
-                          .thumpnail:hover {
+                          .thumpnail-hovered:hover {
                             filter: brightness(60%);
                           }
                         `}
                     </style>
+                    <a
+                        className={'absolute top-80 right-6 cursor-pointer ' + (!hovered && "hidden")}
+                        href={pdf}
+                        target="_blank"
+                    >
+                        <Image src={downloadButton} alt="download"/>
+                    </a>
                 </div>
-
+                <div>
+                </div>
                 <div className="px-8 pt-4 flex justify-between">
                     <div className="font-semibold max-w-xs">{pdf_name}</div>
                     <div className="float-right text-gray-700">Page {parseInt(page) + 1}</div>
