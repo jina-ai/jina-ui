02b8e9721008ae7abb97d7abec9f75bb540d67f3
Roshan Jossy <roshan.jossy@jina.ai>
2021-09-09 18:47:32 +0200

    wip 3d model render

---
+++ packages/showcases/src/pages/gaming.tsx
@@ -0,0 +1,10 @@
+import {mockData} from '../gaming-response-mock-data'
+import Result from '../components/Results'
+
+export default function GamingShowcase () {
+    return (
+        <div>
+            Gaming Showcase
+        </div>
+    )
+}
