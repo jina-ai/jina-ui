b0d05e40de77f894aa57cec860762a09126410ec
Roshan Jossy <roshan.jossy@jina.ai>
2021-09-13 00:30:58 +0200

    fix build failure with webpack config resolve

--- packages/showcases/next.config.js
+++ packages/showcases/next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.fallback = {
+        canvas: false
+      }
+    }
+
+    return config;
+  }
 }
