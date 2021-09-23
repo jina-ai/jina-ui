9e5cb1bb36a92eee08ff219124a6e0c9e4567b7f
Roshan Jossy <roshan.jossy@jina.ai>
2021-09-23 15:45:47 +0200

    remove dist files

--- packages/jinajs/dist/serializer.d.ts
+++
@@ -1,3 +0,0 @@
-import { AnyObject, RawDocumentData, SimpleResponse } from "./types";
-export declare function serializeRequest<IRequest>(documents: RawDocumentData[]): Promise<IRequest>;
-export declare function serializeResponse(response: AnyObject): SimpleResponse;
