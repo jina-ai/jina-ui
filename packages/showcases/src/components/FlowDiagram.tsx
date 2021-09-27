9a0c25fad4276d8841343cf144cfd2b14dc725a9
BastinJafari <bastinjafari@gmail.com>
2021-09-27 13:22:43 +0200

    Style: restyle flowdigram

--- packages/showcases/src/components/FlowDiagram.tsx
+++ packages/showcases/src/components/FlowDiagram.tsx
@@ -3,6 +3,7 @@ import {useRouter} from 'next/router'
 import SampleFlow from "../images/sampleflow.svg";
 import PDFFlow from "../images/pdf-search-flowchart.svg"
 import ECommerceFlow from "../images/e-commerce-flowchart.svg"
+import DragAndDropRec from "../images/dragAndDropRec.svg"
 import github from "../images/github.svg";
 import Image from "next/image";
 import {ChevronUpIcon} from "@heroicons/react/solid";
@@ -50,9 +51,13 @@ export const FlowDiagram = () => {
         setIsOpen(true);
     }
 
+    const [y, setY] = useState(0);
 
     return (
-        <div className={"fixed bottom-0 w-full bg-gray-100 p-4 py-2 " + (!show && "bg-opacity-0")}>
+        <div
+            className={"fixed bottom-0 w-full bg-gray-100 p-4 py-2 rounded-xl"}
+            onClick={() => toggleShow()}
+        >
             <Modal
                 isOpen={modalIsOpen}
                 onRequestClose={() => setIsOpen(false)}
@@ -62,33 +67,30 @@ export const FlowDiagram = () => {
                 <Image src={getFlowChartAsset(path)} alt="flow"/>
 
             </Modal>
-            <div className={"items-center flex fex-row " + (!show && "flex-row-reverse ")}>
-                {show && <a
-                    href="https://get.jina.ai"
-                    rel="noreferrer"
-                    target="_blank"
-                    className="md:mt-2 flex flor-row items-center cursor-pointer hover:bg-gray-200 md:p-2 rounded-md tranistion-all duration-200">
-                    <Image src={github} alt="github"/>
-                    <span className="ml-2">Jina on Github</span>
-                </a>}
-                {show && <div className="md:text-lg md:mt-2 flex-1 font-medium text-center">How it works</div>}
-                <ChevronUpIcon
-                    onClick={toggleShow}
-                    className={`md:mt-2 h-10 p-2 rounded-full hover:bg-gray-200 cursor-pointer transition-all duration-200 transform ${
-                        show ? "rotate-180" : "rotate-0"
-                    }`}
-                />
+            <div className="flex justify-center">
+                <Image src={DragAndDropRec}/>
             </div>
+            <div className="md:text-lg md:mt-2 flex-1 font-medium">Flow diagram</div>
+
             <div
                 className={`overflow-hidden transition-all duration-200 ease-in-out ${
                     show ? "max-h-screen" : "max-h-0"
                 }`}
             >
-                <div className="flex justify-center pb-8 bg-gray-100 rounded mt-4 overflow-y-auto" onClick={() => setIsOpen(true)}>
+                <div className="flex justify-center pb-8 bg-gray-100 rounded mt-4 overflow-y-auto"
+                     onClick={() => setIsOpen(true)}>
                     <Image src={getFlowChartAsset(path)} alt="flow"/>
 
                 </div>
             </div>
+            {show && <a
+                href="https://get.jina.ai"
+                rel="noreferrer"
+                target="_blank"
+                className="md:mt-2 flex flor-row items-center cursor-pointer hover:bg-gray-200 md:p-2 rounded-md tranistion-all duration-200 float-right">
+                <Image src={github} alt="github"/>
+                <span className="ml-2">Jina on Github</span>
+            </a>}
         </div>
     );
 };
