92a666f3078290bc16ead76735ec293b967521b4
BastinJafari <bastinjafari@gmail.com>
2021-09-27 12:38:57 +0200

    feat: bigger flowdiagram onClick

--- packages/showcases/src/components/FlowDiagram.tsx
+++ packages/showcases/src/components/FlowDiagram.tsx
@@ -6,6 +6,7 @@ import ECommerceFlow from "../images/e-commerce-flowchart.svg"
 import github from "../images/github.svg";
 import Image from "next/image";
 import {ChevronUpIcon} from "@heroicons/react/solid";
+import Modal from 'react-modal';
 
 const getFlowChartAsset = (showcase: string) => {
     switch (showcase) {
@@ -18,19 +19,49 @@ const getFlowChartAsset = (showcase: string) => {
     }
 }
 
+const customStyles = {
+    overlay: {
+        background: "black"
+    },
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
 
 
 export const FlowDiagram = () => {
     const [show, setShow] = useState(true);
     const {asPath} = useRouter()
     const path = asPath.replace('/', '')
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
 
     function toggleShow() {
         setShow((prev) => !prev);
     }
 
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+
     return (
         <div className={"fixed bottom-0 w-full bg-gray-100 p-4 py-2 " + (!show && "bg-opacity-0")}>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={() => setIsOpen(false)}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <Image src={getFlowChartAsset(path)} alt="flow"/>
+
+            </Modal>
             <div className={"items-center flex fex-row " + (!show && "flex-row-reverse ")}>
                 {show && <a
                     href="https://get.jina.ai"
@@ -53,7 +84,7 @@ export const FlowDiagram = () => {
                     show ? "max-h-screen" : "max-h-0"
                 }`}
             >
-                <div className="flex justify-center pb-8 bg-gray-100 rounded mt-4 overflow-y-auto">
+                <div className="flex justify-center pb-8 bg-gray-100 rounded mt-4 overflow-y-auto" onClick={() => setIsOpen(true)}>
                     <Image src={getFlowChartAsset(path)} alt="flow"/>
 
                 </div>
