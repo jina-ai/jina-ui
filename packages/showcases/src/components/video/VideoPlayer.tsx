e85c03453e092e0cf841d50ee0af35d655a3789b
Roshan Jossy <roshan.jossy@jina.ai>
2021-10-11 16:55:17 +0200

    wip: timestamp typo

--- packages/showcases/src/components/video/VideoPlayer.tsx
+++ packages/showcases/src/components/video/VideoPlayer.tsx
@@ -3,10 +3,9 @@ import ReactPlayer from "react-player"
 
 type VideoPlayerProps = {
     result: any
-    timeStamp?: number
 }
-export default function VideoPlayer ({result, timeStamp}: VideoPlayerProps) {
+export default function VideoPlayer ({result}: VideoPlayerProps) {
     return (
-        <ReactPlayer url={`${result.url}&t=${result.timeStamp || 0}`} width="100%" height="20rem" controls />
+        <ReactPlayer url={`${result.url}&t=${result.timestamp}s`} width="100%" height="20rem" controls />
     )
 }
